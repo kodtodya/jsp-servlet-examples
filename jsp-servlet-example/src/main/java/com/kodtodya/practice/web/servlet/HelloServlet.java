2686657efd122afa201c63a67bf2ab4faf2363c2
Avadhut <kodtodya@gmail.com>
2024-02-18 09:41:22 +0530

    initial import

---
+++ jsp-servlet-example/src/main/java/com/kodtodya/practice/web/servlet/HelloServlet.java
@@ -0,0 +1,33 @@
+package com.kodtodya.practice.web.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.doGet(request, response);
+    }
+
+    public void destroy() {
+    }
+}
